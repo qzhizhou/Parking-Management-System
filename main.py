9dd3df3e0af7377cc5e5f8d1b0c4a6c89eed7aae
ruotianliu <43013253+ruotianliu@users.noreply.github.com>
2018-11-04 15:35:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ car_path = enter a car path
 license_plate_path = enter your save path
 json_path = enter your json path
 
-extraction.store_license_plate(car_path)
+extraction.store_license_plate(car_path,license_plate_path)
 print(extraction.time_extraction(car_path))
 text.recongnize(license_plate_path,json_path)
