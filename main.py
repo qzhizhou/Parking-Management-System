62f3da5b5ad04d5f1b4583c7a2195e1471307a87
Gang Wei <43114190+WayGang@users.noreply.github.com>
2018-11-12 12:51:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import pandas as pd
 import numpy as np
 import MySQLdb'''
 
-car_path = "/Users/gangwei/Desktop/smartparker/car_image/car2.jpg"
-license_plate_path = '/Users/gangwei/Desktop/smartparker/license_image/license2.jpg'
+car_path = "/Users/gangwei/Desktop/smartparker/car_out_image/car6.jpg"
+license_plate_path = '/Users/gangwei/Desktop/smartparker/license_image/license6.jpg'
 json_path = "/Users/gangwei/Desktop/smartparker/textdetect-52e8d68b62cc.json"
 
 upd_time = extraction.time_extraction(car_path)
@@ -18,15 +18,15 @@ license_id = text.recognize(license_plate_path, json_path)
 print(upd_time)
 print(license_id)
 print("Processing Database")
-balance = 300
+
 
 
 def entry_cars_all():
 
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
     cursor = db.cursor()
-    sql = "INSERT INTO cars_all(license_id, entry_time, balance) VALUES(\'%s\', \'%s\', \'%s\')" % (
-        license_id, upd_time, balance)
+    sql = "INSERT INTO cars_all(license_id, entry_time, image_path) VALUES(\'%s\', \'%s\', \'%s\')" % (
+        license_id, upd_time, car_path)
 
     try:
         cursor.execute(sql)
@@ -38,8 +38,8 @@ def entry_cars_all():
 def entry_cars_in():
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
     cursor = db.cursor()
-    sql = "INSERT INTO cars_in(license_id, entry_time, balance) VALUES(\'%s\', \'%s\', \'%s\')" % (
-        license_id, upd_time, balance)
+    sql = "INSERT INTO cars_in(license_id, entry_time) VALUES(\'%s\', \'%s\')" % (
+        license_id, upd_time)
     try:
         cursor.execute(sql)
         db.commit()
@@ -48,6 +48,7 @@ def entry_cars_in():
         db.close()
 
 def entry_cars_balance():
+    balance = input("Give me the money:")
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
     cursor = db.cursor()
     sql = "INSERT INTO cars_balance(license_id, balance) VALUES(\'%s\', \'%s\')" % (
@@ -65,12 +66,12 @@ def  out_cars_all():
     cursor = db.cursor()
     sql = "UPDATE cars_all SET exit_time = '%s' WHERE license_id = '%s'" % (upd_time,license_id)
 
-    #try:
-    cursor.execute(sql)
-    db.commit()
-    '''except:
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
         db.rollback()
-        db.close()'''
+        db.close()
 
 def out_cars_in():
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
@@ -95,12 +96,33 @@ def out_cars_balance():
         db.rollback()
         db.close()
 
+def query():
+    db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
+    cursor = db.cursor()
+    #l_id = input("The license you want to find is: ")
+    sql = "SELECT * FROM cars_all \
+           WHERE No. = '%s'" % (7)
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            license_id = row[1]
+            entry_time = row[2]
+            exit_time = row[3]
+            image_path = row[5]
+            print("license_id='%s',entry_time='%s',exit_time='%s',image_path='%s'" % \
+                  (license_id, entry_time, exit_time, image_path))
+    except:
+        print("Error: unable to fetch data")
+    db.close()
+
 #if(entry==1):
 #entry_cars_all()
 #entry_cars_in()
 #entry_cars_balance()
 
 #if(out==1):
-#out_cars_in()
-#out_cars_all()
-#out_cars_balance()
+out_cars_in()
+out_cars_all()
+out_cars_balance()
+#query()
