3e42604ca0edc0d86a07a2ce839ac5614ed9377f
Gang Wei <43114190+WayGang@users.noreply.github.com>
2018-11-26 22:50:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from license_plate_extraction import extraction
-from googlevision import *
+from license_image_extraction import extraction
+from recognize_license_id import *
 import pymysql
 import pymysql
 
@@ -8,14 +8,17 @@ import pandas as pd
 import numpy as np
 import MySQLdb'''
 
-car_path = "/Users/gangwei/Desktop/smartparker/car_out_image/car6.jpg"
-license_plate_path = '/Users/gangwei/Desktop/smartparker/license_image/license6.jpg'
+car_path = "/Users/gangwei/Desktop/smartparker/car_in_image/car2.jpg"
+license_plate_path = '/Users/gangwei/Desktop/smartparker/license_image/license.jpg'
 json_path = "/Users/gangwei/Desktop/smartparker/textdetect-52e8d68b62cc.json"
 
 upd_time = extraction.time_extraction(car_path)
 extraction.store_license_plate(car_path,license_plate_path)
-license_id = text.recognize(license_plate_path, json_path)
-print(upd_time)
+license_id = str(text.recognize(license_plate_path, json_path))
+license_id = license_id.replace("\n","")
+license_id = license_id.replace(" ","")
+license_id = license_id.replace("·","")
+#print(upd_time)
 print(license_id)
 print("Processing Database")
 
@@ -72,7 +75,6 @@ def  out_cars_all():
     except:
         db.rollback()
         db.close()
-
 def out_cars_in():
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
     cursor = db.cursor()
@@ -93,36 +95,78 @@ def out_cars_balance():
         cursor.execute(sql)
         db.commit()
     except:
-        db.rollback()
-        db.close()
+        db.rollbapymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
+    cursor = db.cursor()
 
-def query():
+def query_all():
+    input = input()
     db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
     cursor = db.cursor()
-    #l_id = input("The license you want to find is: ")
-    sql = "SELECT * FROM cars_all \
-           WHERE No. = '%s'" % (7)
+    # l_id = input("The license you want to find is: ")
+    sql = "SELECT * FROM cars_all WHERE LICENSE_ID = '%s'" %(input)
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
+        #print(results)
         for row in results:
             license_id = row[1]
             entry_time = row[2]
             exit_time = row[3]
-            image_path = row[5]
-            print("license_id='%s',entry_time='%s',exit_time='%s',image_path='%s'" % \
+            image_path = row[4]
+            print("license_id='%s'\nentry_time='%s'\nexit_time='%s'\nimage_path='%s'" % \
                   (license_id, entry_time, exit_time, image_path))
     except:
         print("Error: unable to fetch data")
     db.close()
 
+def query_balance():
+    db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
+    cursor = db.cursor()
+    # l_id = input("The license you want to find is: ")
+    sql = "SELECT * FROM cars_balance WHERE LICENSE_ID = '%s'" %(input)
+
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            license_id = row[0]
+            balance = row[1]
+            print("license_id='%s'\nbalance='%s'\n" % \
+                  (license_id, balance))
+    except:
+        print("Error: unable to fetch data")
+    db.close()
+
+def query_in():
+    db = pymysql.connect("localhost", "root", "WeiGang0502", "smartparker")
+    cursor = db.cursor()
+    # l_id = input("The license you want to find is: ")
+    sql = "SELECT * FROM cars_in WHERE LICENSE_ID = '%s'" %(input)
+
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        print("results = ",results)
+        for row in results:
+            license_id = row[0]
+            entry_time = row[1]
+            print("license_id='%s'\nentry_time='%s'\n" % \
+                  (license_id, entry_time))
+    except:
+        print("Error: unable to fetch data")
+    db.close()
 #if(entry==1):
 #entry_cars_all()
 #entry_cars_in()
 #entry_cars_balance()
 
 #if(out==1):
-out_cars_in()
-out_cars_all()
-out_cars_balance()
-#query()
+#out_cars_in()
+#out_cars_all()
+#out_cars_balance()
+
+#query_all()
+#query_balance()
+#query_in()
+
+
